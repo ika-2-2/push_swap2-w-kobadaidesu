fce0b7aae8b6e62a98d692efb4ac606e25558001
ika-2-2 <219417869+ika-2-2@users.noreply.github.com>
2026-06-05 23:35:07 +0900

    feat: 座標圧縮を実装

--- compression.c
+++ compression.c
@@ -6,17 +6,31 @@
 /*   By: kasuzuki <kasuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 14:54:35 by kasuzuki          #+#    #+#             */
-/*   Updated: 2026/06/04 16:10:17 by kasuzuki         ###   ########.fr       */
+/*   Updated: 2026/06/05 23:33:20 by kasuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int *coordinate_comp(char **argv)
+// 座標圧縮
+int *coordinate_comp(t_stack *stack_a)
 {
-    char c;
-    while(!*argv)
+    t_stack *current_a;
+    t_stack *current_b;
+    int cnt;
+    
+    current_a = stack_a;
+    while(current_a)
     {
-        
+        cnt = 0;
+        current_b = stack_a;
+        while(current_b)
+        {
+            if(current_a->value > current_b->value)
+                cnt++;
+            current_b = current_b->next;
+        }
+        current_a->index = cnt;
+        current_a = current_a->next;
     }
 }
