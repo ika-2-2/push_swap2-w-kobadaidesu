46994cc7c1e0a1211ac2d374dfb279620f6ada5d
ika-2-2 <219417869+ika-2-2@users.noreply.github.com>
2026-06-05 23:21:39 +0900

    CI

--- push_swap.h
+++ push_swap.h
@@ -19,16 +19,16 @@ long				ft_atol(char *str);
 void				error_check(char **args);
 t_stack				*init_stack(char **args);
 
-void sa(t_stack **stack_a);
-void sb(t_stack **stack_b);
-void ss(t_stack **stack_a, t_stack **stack_b);
-void pa(t_stack **stack_a, t_stack **stack_b);
-void pb(t_stack **stack_a, t_stack **stack_b);
-void ra(t_stack **stack_a);
-void rb(t_stack **stack_b);
-void rr(t_stack **stack_a, t_stack **stack_b);
-void rra(t_stack **stack_a);
-void rrb(t_stack **stack_b);
-void rrr(t_stack **stack_a, t_stack **stack_b);
+void				sa(t_stack **stack_a);
+void				sb(t_stack **stack_b);
+void				ss(t_stack **stack_a, t_stack **stack_b);
+void				pa(t_stack **stack_a, t_stack **stack_b);
+void				pb(t_stack **stack_a, t_stack **stack_b);
+void				ra(t_stack **stack_a);
+void				rb(t_stack **stack_b);
+void				rr(t_stack **stack_a, t_stack **stack_b);
+void				rra(t_stack **stack_a);
+void				rrb(t_stack **stack_b);
+void				rrr(t_stack **stack_a, t_stack **stack_b);
 
 #endif
