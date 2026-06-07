acbdf615dc79d1a0449daa70c26766877871a5af
ika-2-2 <219417869+ika-2-2@users.noreply.github.com>
2026-06-07 19:08:37 +0900

    feat: rotate系作成

--- rotate.c
+++ rotate.c
@@ -6,10 +6,33 @@
 /*   By: kasuzuki <kasuzuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/06 20:22:56 by kasuzuki          #+#    #+#             */
-/*   Updated: 2026/06/06 20:29:26 by kasuzuki         ###   ########.fr       */
+/*   Updated: 2026/06/07 19:07:59 by kasuzuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ra(t_stack **stack_a, int flg)
+{
+    if (!stack_a || !*stack_a)
+        return ;
+	*stack_a = (*stack_a)->next;
+	if (flg == 0)
+		write(1, "ra\n", 3);
+}
 
+void	rb(t_stack **stack_b, int flg)
+{
+    if (!stack_b || !*stack_b)
+        return ;
+	*stack_b = (*stack_b)->next;
+	if (flg == 0)
+		write(1, "rb\n", 3);
+}
+
+void	rr(t_stack **stack_a, t_stack **stack_b)
+{
+	ra(stack_a, 1);
+	rb(stack_b, 1);
+	write(1, "rr\n", 3);
+}
