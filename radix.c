2ee15ddd0d9c4d94cea20c340332609e2fb222ec
ika-2-2 <219417869+ika-2-2@users.noreply.github.com>
2026-06-18 20:14:41 +0900

    feat: radixまとめる関数の仕上げ

--- radix.c
+++ radix.c
@@ -6,46 +6,58 @@
 /*   By: ika_eater <ika_eater@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/15 20:56:05 by ika_eater         #+#    #+#             */
-/*   Updated: 2026/06/16 00:07:15 by ika_eater        ###   ########.fr       */
+/*   Updated: 2026/06/18 20:13:54 by ika_eater        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void radix_check(t_stack **stack_a, t_stack **stack_b, int size, int bit)
+void	radix_check(t_stack **stack_a, t_stack **stack_b, int size, int bit)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while(i < size)
-    {
-        if((((*stack_a)->index >> bit) & 1) == 0)
-            pb(stack_a, stack_b);
-        else
-            ra(stack_a, 0);
-        i++;
-    }
+	i = 0;
+	while (i < size)
+	{
+		if ((((*stack_a)->index >> bit) & 1) == 0)
+			pb(stack_a, stack_b);
+		else
+			ra(stack_a, 0);
+		i++;
+	}
+	while (*stack_b)
+        pa(stack_a, stack_b);
 }
 
-int stack_size(t_stack **stack_a)
+int	stack_size(t_stack **stack_a)
 {
-    t_stack *a;
-    int size;
-    
-    a = *stack_a;
-    size = 0;
-    while(size != a->index)
-    {
-        size = a->index;
-        a = a->next;
-    }
-    return (size);
+	t_stack	*a;
+	int		size;
+
+	if (!stack_a)
+		return (0);
+	a = (*stack_a)->next;
+	size = 0;
+	while (a != stack_a)
+	{
+		size++;
+		a = a->next;
+	}
+	return (size + 1);
 }
 
-int radix_sort(t_stack **stack_a, t_stack **stack_b)
+void	radix_sort(t_stack **stack_a, t_stack **stack_b)
 {
-    int bit;
-    int size;
+	int	bit;
+	int	size;
+    int max_index;
 
-    
+    size = stack_size(stack_a);
+    bit = 0;
+    max_index - size - 1;
+    while ((max_index >> bit) != 0)
+    {
+        radix_check(stack_a, stack_b, size, bit);
+        bit++;
+    }
 }
